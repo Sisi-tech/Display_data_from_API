417e8ce80c2f77dc75f46668941229c93978ad98
sisi wang <sisiwang242@gmail.com>
2023-07-29 22:41:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import {useState, useEffect} from "react"
+
+function Github({name, bio, followers, following, avatar_url}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{bio}</p>
+      <p>Followers: {followers}</p>
+      <p>Following: {following}</p>
+      <img src={avatar_url} height={150}/>
+    </div>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(
+      'https://api.github.com/users/Sisi-tech'
+    ).then((response) => response.json()).then(setData);
+  }, []);
+  if(data)
+  return <Github 
+  name={data.name} 
+  bio={data.bio} 
+  followers={data.followers} 
+  following={data.following}
+  avatar_url={data.avatar_url}
+  />;
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
 export default App;
