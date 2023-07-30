ffa41bc1f392a8959f23266014d87119c1f64586
sisi wang <sisiwang242@gmail.com>
2023-07-30 12:04:17 -0400

    handing loading states

--- src/App.js
+++ src/App.js
@@ -15,11 +15,24 @@ function Github({name, bio, followers, following, avatar_url}) {
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch(
       'https://api.github.com/users/Sisi-tech'
-    ).then((response) => response.json()).then(setData);
+    )
+    .then((response) => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
   }, []);
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <pre>{JSON.stringify(error)}</pre>
+  if (!data) return null;
+
   if(data)
   return <Github 
   name={data.name} 
